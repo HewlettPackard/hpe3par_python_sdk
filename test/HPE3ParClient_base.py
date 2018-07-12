b0b9487249f325755f42fd41c6ef59bd6ce23a56
Farhan Nomani <nomani@hpe.com>
2018-07-12 02:38:14 -0400

    Fix for broken unit tests

--- test/HPE3ParClient_base.py
+++ test/HPE3ParClient_base.py
@@ -125,12 +125,8 @@ class HPE3ParClientBaseTestCase(unittest.TestCase):
             except Exception:
                 pass
 
-            time.sleep(5)
-            if self.withFilePersona:
-		pass
-                #self.cl = file_client.HPE3ParFilePersonaClient(self.flask_url)
-            else:
-                self.cl = client.HPE3ParClient(self.flask_url)
+            time.sleep(1)
+            self.cl = client.HPE3ParClient(self.flask_url)
 
             if self.withSSH:
 
@@ -209,10 +205,8 @@ class HPE3ParClientBaseTestCase(unittest.TestCase):
 
         if not self.port:
             ports = self.cl.getPorts()
-	    print "hello"
-	    print vars(self.cl)
-	    for port in ports:
-	        print port.mode
+        for port in ports:
+            #print port.mode
             ports = [port for port in ports if port.linkState == 4 and  ( port.device is not None or not port.device) and port.mode == 2]
             self.port = ports[0].port_pos
 
