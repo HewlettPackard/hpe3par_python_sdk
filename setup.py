2d87bc0ee775c08216fc3a084b2e019f16f1c989
Sneha Rai <33340489+sneharai4@users.noreply.github.com>
2019-09-09 19:02:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ try:
 except ImportError:
     from distutils.core import setup, find_packages
 
-with open('README.md', 'r') as file:
+with open('README.rst', 'r') as file:
     readme = file.read()
 
 setup(
